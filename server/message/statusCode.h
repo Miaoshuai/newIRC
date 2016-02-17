c7a677bfb34d273628f2c7d29f38f203f6c94bcb
miaoshua <945970809@qq.com>
2016-02-17 23:38:38 +0800

    消息处理的状态码

---
+++ server/message/statusCode.h
@@ -0,0 +1,21 @@
+/*======================================================
+    > File Name: statusCode.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年02月17日 星期三 20时58分58秒
+ =======================================================*/
+#pragma once
+
+enum REGISTER
+{
+    SUCCEED = 1,
+    FAIL = -1   
+};
+
+enum LOGIN
+{
+    SUCCEED = 1,
+    FAIL = -1
+};
+
