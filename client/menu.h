aab0e3dcdaf740bdb63ef21f0a6d1f7a2fa44a07
miaoshua <945970809@qq.com>
2016-03-04 13:33:04 +0800

    菜单

---
+++ client/menu.h
@@ -0,0 +1,24 @@
+/*======================================================
+    > File Name: menu.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年03月01日 星期二 23时39分27秒
+ =======================================================*/
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+
+void menu(void)
+{
+    printf("\t\t\t用户主界面\n\n");
+    printf("\t\t1.登陆\n");
+    printf("\t\t2.注册\n");
+    printf("\t\t3.房间\n");
+    printf("\t\t3.退出\n");
+}
+
+
