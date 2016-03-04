d96d9e83846ccd54062d66a19ac6e8cc11fb5882
miaoshua <945970809@qq.com>
2016-03-04 16:54:54 +0800

    新增具体操作选择的枚举

--- client/menu.h
+++ client/menu.h
@@ -12,13 +12,27 @@
 
 #include <stdio.h>
 
-void menu(void)
+enum BUTTON
+{
+    LOGIN = 1,
+    REGISTER,
+    REGISTER_ROOM,
+    ENTER_ROOM,
+    QUIT
+};
+
+int menu(void)
 {
     printf("\t\t\t用户主界面\n\n");
     printf("\t\t1.登陆\n");
     printf("\t\t2.注册\n");
-    printf("\t\t3.房间\n");
-    printf("\t\t3.退出\n");
+    printf("\t\t3.注册房间\n");
+    printf("\t\t4.进入房间\n");
+    printf("\t\t5.退出\n");
+
+    int button;
+    std::cin>>button;
+    return button;
 }
 
 
