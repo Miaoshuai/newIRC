5afc5c40074830e09cc5fe11bb12c28fa70465e3
miaoshua <945970809@qq.com>
2016-03-04 13:36:07 +0800

    处理结果的状态码

---
+++ client/statusCode.h
@@ -0,0 +1,29 @@
+/*======================================================
+    > File Name: statusCode.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年02月17日 星期三 20时58分58秒
+ =======================================================*/
+#pragma once
+
+enum REGISTER
+{
+    REGISTER_SUCCEED = 1,
+    REGISTER_FAIL = -1   
+};
+
+enum LOGIN
+{
+    LOGIN_SUCCEED = 2,
+    LOGIN_FAIL = -2
+};
+
+enum ROOM
+{
+    C_ROOM_SUCCEED = 3,
+    C_ROOM_FAIL = -3,
+    E_ROOM_SUCCEED = 4,
+    E_ROOM_FAIL = -4
+};
+
